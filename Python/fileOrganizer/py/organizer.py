7bd31c58d06e020d78a90feaa7c286f6dc5da85f
ilhamsyamsuddin <ilham5442@gmail.com>
2020-08-05 22:12:03 +0700

    selesai organizer

--- Python/fileOrganizer/py/organizer.py
+++ Python/fileOrganizer/py/organizer.py
@@ -4,7 +4,7 @@ try:
     for (path,dirs,files) in os.walk(path):
         for file in files:
            extension = file.split('.')[1]
-           if os.path.exists(r"E:\\Sehari-hari\\Proyek\\Python\\fileOrganizer\\"+extension):
+           if os.path.exists(path + r"\\"+ extension):
                if file.endswith(extension):
                    shutil.move(file,"E:\\Sehari-hari\\Proyek\\Python\\fileOrganizer\\"+extension)
            else:
