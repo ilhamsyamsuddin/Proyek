1e91c107e1ce5f99c15a8cca8495cbf76cf8c02b
ilhamsyamsuddin <ilham5442@gmail.com>
2020-08-07 00:56:24 +0700

    finished pdfpassword

--- Python/PDFpassword/passPDF.py
+++ Python/PDFpassword/passPDF.py
@@ -5,3 +5,10 @@ pdfwriter = PdfFileWriter()
 pdf = PdfFileReader("TAHA_Solution.pdf")
 for page_num in range(pdf.numPages):
     pdfwriter.addPage(pdf.getPage(page_num))
+
+password = "Omnissiah"
+pdfwriter.encrypt(password)
+
+with open("TAHA_Solution_encrypted.pdf",'wb') as f:
+    pdfwriter.write(f)
+    f.close()
